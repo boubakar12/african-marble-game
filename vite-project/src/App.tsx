db9c4b65225b7964efa3d8961620a5b2ebd241c8
Boubakar Diallo <bd453@cornell.edu>
2026-02-13 13:46:39 -0500

    use Hashrouter

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,33 +1,20 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Level1Page from "./pages/Level1Page";
 import Level2Page from "./pages/Level2Page";
 import Level3Page from "./pages/Level3Page";
 import NotFound from "./pages/NotFound";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/level/1" element={<Level1Page />} />
-          <Route path="/level/2" element={<Level2Page />} />
-          <Route path="/level/3" element={<Level3Page />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/level/1" element={<Level1Page />} />
+        <Route path="/level/2" element={<Level2Page />} />
+        <Route path="/level/3" element={<Level3Page />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HashRouter>
+  );
+}
