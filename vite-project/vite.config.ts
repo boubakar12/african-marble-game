0d5385ba9ff5e42016ebd664bdc4e3284cb40341
Boubakar Diallo <bd453@cornell.edu>
2026-02-13 05:57:51 -0500

    Update vite.config.ts

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
+  base: "/african-marble-game/", // <-- IMPORTANT (repo name)
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+  server: {
+    host: "::",
+    port: 8080,
+  },
+});
